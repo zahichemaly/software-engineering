03ffea361592025380f87634693dab9cf57f0082
naihchams <hamdannai16@gmail.com>
2023-09-23 14:26:43 +0300

    ex3

---
+++ naihamdan-220607/tp5/NewsBoard/NewsBoard.Business/Models/DeleteNewsCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsBoard.Business.Models
+{
+    public class DeleteNewsCommand:IRequest<string>
+    {
+        public  string NewsId { get; set; }
+    }
+}
