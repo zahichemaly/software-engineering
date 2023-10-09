589043294bc7d1224f78530096b06b18e3de20c5
maria.hourani <maria.hourani@net.usj.edu.lb>
2023-10-09 09:56:25 +0300

    committed changes

---
+++ MariaHourani_222310/tp2/GachaSystem/Banner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public class Banner
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public List<GachaItem> Items { get; set; }
+    }
+}
