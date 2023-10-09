7a6c48f3fc302af4d1b508fac93178bc7cd6aada
Hadi cheayto <hadi.ch.2003@gmail.com>
2023-10-09 10:39:42 +0300

    Tp2

---
+++ hadiCheayto_221008/Tp2/GashaSystem/GashaSystem/PermanentBanner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GashaSystem
+{
+    public class PermanentBanner : Banner
+    {
+        public PermanentBanner(int id, string name, List<GachaItem> items, int cost) : base(id, name, items, cost)
+        {
+        }
+    }
+}
---
+++ hadiCheayto_221008/Tp2/GashaSystem/GashaSystem/System.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GashaSystem
+{
+    public class System
+    {
+        private List<BaseUser> Users;
+        private int Version;
+    }
+}
