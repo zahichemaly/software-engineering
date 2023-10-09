80c91ae7cb8890536605c556dd6ca37d136690bb
Wissam_Nasreddine <wissam.nasreddine@net.usj.edu.lb>
2023-10-09 09:57:14 +0300

    Added class diagram project

---
+++ WissamNasreddine_222074/tp2/GachaSystem/GachaSystem/admin_user.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public class admin_user : BaseUser
+    {
+        public void update_permanent_banner()
+        {
+
+        }
+
+        public void Modify_exclusive_banner()
+        {
+
+        }
+
+        public void Manage_users()
+        {
+
+        }
+        public void update_system_version()
+        {
+
+        }
+    }
+}
