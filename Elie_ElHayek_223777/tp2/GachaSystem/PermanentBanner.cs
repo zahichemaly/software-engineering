5a58e3940d0a6d508d7b35937767daa689140ebb
elieHayek <elie.hayek2@net.usj.edu.lb>
2023-10-09 10:40:45 +0300

    tp2-part1

---
+++ Elie_ElHayek_223777/tp2/GachaSystem/PermanentBanner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public class PermanentBanner:Banner
+    {
+        public int ID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public List<GachaItem> Items
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Cost
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+
+}
