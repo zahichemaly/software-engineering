235eff1f780bbb88a4fc77522ff026a4b1bdc049
maria.hourani <maria.hourani@net.usj.edu.lb>
2023-10-09 10:24:55 +0300

    commit

---
+++ MariaHourani_222310/tp2/GachaSystem/Version.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public class Version
+    {
+        private int Version_number;
+        private int Version_Code;
+        private DateTime Date;
+    }
+}
