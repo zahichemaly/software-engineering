5574400cc96dbe3a843fa20522e7ad4c47b758b8
jad <jad.moussa@net.usj.edu.lb>
2023-10-09 15:00:13 +0300

    upload tp2

---
+++ jad_moussa_191685/tp2/GochaSystem/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public abstract class BaseUser : IAccount
+    {
+        public Status UserStatus; 
+        public abstract void validate();
+        public int ID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string FirstName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string LastName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+        public DateTime DateOfBirth { get; set; }
+        public string GetFullName()
+        {
+           return this.FirstName + " " + this.LastName; 
+        }
+
+        public int GetAge()
+        {
+
+            return -1; 
+        }
+
+    }
+}
