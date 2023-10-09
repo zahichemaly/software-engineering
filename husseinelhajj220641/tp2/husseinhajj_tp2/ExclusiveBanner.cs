7876b2dd116ed272d9c7115ccb9e2862b82ac46b
Your Name <you@example.com>
2023-10-09 10:46:33 +0300

    tp2

---
+++ husseinelhajj220641/tp2/husseinhajj_tp2/ExclusiveBanner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace husseinhajj_tp2
+{
+    public class ExclusiveBanner:Banner
+    {
+        
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
---
+++ husseinelhajj220641/tp2/husseinhajj_tp2/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace husseinhajj_tp2
+{
+    public enum Status
+    {
+        Active,
+        PendingActivation,
+        Banned
+    }
+}
