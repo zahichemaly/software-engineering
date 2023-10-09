88663e70da19729b4056fb5b6d7f7460281eb541
Ali fawaz <Afawaz11@outlook.com>
2023-10-09 16:08:39 +0300

    SRS.txt added

---
+++ Alifawaz-212694/tp2/GachaSystem/GachaSystem/PullHistory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public class PullHistory : BaseUser
+    {
+        public Gacha Item { get; set; }
+        public ExclusiceBanner Banner { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int PullNumber { get; set; }
+
+        public PullHistory(Gacha item, ExclusiceBanner banner, DateTime creationDate, int pullNumber)
+        {
+            Item = item;
+            Banner = banner;
+            CreationDate = creationDate;
+            PullNumber = pullNumber;
+        }
+    }
+}
