ab44be4a81a7dd64b379d5b7609d83d5274955dd
Elie-Haouchan <elie.haouchan@net.usj.edu.lb>
2023-10-09 10:42:32 +0300

    Added classes and methods based on requirements.

---
+++ ElieHaouchan-220561/tp2/GachaSystem/PermanentBanner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public class PermanentBanner : Banner
+    {
+
+        
+
+        public List<GachaItem> PermanentItems
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+      
+        public GachaItem GachaItem
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public override void IsPullAllowed(Player player)
+        {
+            //Implementation of the abstract method to check if a player can pull from this permanent banner.
+        }
+
+
+    }
+}
