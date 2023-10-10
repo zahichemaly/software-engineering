194dad30012997bf726766814ab847fe7fc1d56e
naihchams <hamdannai16@gmail.com>
2023-10-10 11:09:54 +0300

    TP2-part1

---
+++ naihamdan-220607/tp2/GachaSystem/GachaSystem/Admin.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public class Admin:BaseUser
+    {
+        public int ID
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string FirstName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public string LastName
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Balance
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Pulls
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void updatePermanentBanner(PermanentBanner banner) { }
+
+        public void AddExclusiveBanner() { }
+
+        public void UpdateExclusiveBanner(ExclusiveBanner banner) { }
+
+        public void DeleteExclusiveBanner(ExclusiveBanner banner) { }
+
+        public void AddUser() { }
+
+        public void RemoveUser(BaseUser user) { }
+
+        public void UpdateSystemVersion(System system) { }
+
+    }
+}
