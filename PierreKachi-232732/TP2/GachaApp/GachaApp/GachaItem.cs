0503ccc0552f4962f4cfb709be1f06ad0b4039f6
Pierre K <kachi.pierre@gmail.com>
2023-10-15 21:46:41 +0300

    never playing genshin impact again

--- PierreKachi-232732/TP2/GachaApp/GachaApp/GachaItem.cs
+++ PierreKachi-232732/TP2/GachaApp/GachaApp/GachaItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace GachaApp
@@ -9,10 +10,51 @@ namespace GachaApp
     {
         private int ID;
         private String Name;
-        private enum Rarity { 
+
+        public enum Rarity
+        {
             ThreeStar,
             FourStar,
             FiveStar
         };
+        private Rarity rarity;
+        public int getId(){
+            return ID;
+        }
+        public GachaItem(string name, int id, int rarity)
+        {
+            this.Name = name;
+            this.ID = id;
+            switch (rarity)
+            {
+                case 3:
+                    this.rarity = Rarity.ThreeStar;
+                    break;
+                case 4:
+                    this.rarity = Rarity.FourStar;
+                    break;
+                case 5:
+                    this.rarity = Rarity.FiveStar;
+                    break;
+
+            }
+        
+        }
+        public GachaItem() { }
+        public int getRarity(){
+            switch (rarity)
+            {
+                case Rarity.ThreeStar:
+                    return 3;
+                case Rarity.FourStar:
+                    return 4;
+                case Rarity.FiveStar:
+                    return 5;
+                default:
+                    return 0;
+            }
+            
+
+        }
     }
 }
--- PierreKachi-232732/TP2/GachaApp/GachaApp/History.cs
+++ PierreKachi-232732/TP2/GachaApp/GachaApp/History.cs
@@ -5,11 +5,37 @@ using System.Text;
 
 namespace GachaApp
 {
-    public class History
-    {
+    public class historyItem
+    { 
         private GachaItem item;
         private Banner RelatedBanner;
         private DateTime pullDate;
         private int pullNumber;
+
+        public GachaItem GetItem()
+    {
+        return item;
+    }
+
+    public Banner GetRelatedBanner()
+    {
+        return RelatedBanner;
+    }
+
+    public DateTime GetPullDate()
+    {
+        return pullDate;
+    }
+
+    public int GetPullNumber()
+    {
+        return pullNumber;
+    }
+        public historyItem(GachaItem item, Banner banner, DateTime pullDate, int pullNumber){
+            this.item = item;
+            this.RelatedBanner = banner;
+            this.pullDate = pullDate;
+            this.pullNumber = pullNumber;
+        }
     }
 }
