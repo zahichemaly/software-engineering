b4a57a4ffcca015cee0acb26d520518c91861e08
Pamela BouKhalil <pamela.boukhalil@net.usj.edu.lb>
2023-10-15 15:51:00 +0300

    Added the activity diagram and part 3 of tp2 NB: the gacha.txt file is in the project's folder this is the complete path: Gachsystems/Gachsystem/bin/debug/net6.0

--- Pamela_BouKhalil_220199/tp2/GachaSystem/GachaSystem/Player.cs
+++ Pamela_BouKhalil_220199/tp2/GachaSystem/GachaSystem/Player.cs
@@ -1,26 +1,112 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using GachaSystem;
 
-namespace GachaSystem
+public class Player
 {
-    public class Player : BaseUser
+    public int ID { get; set; } 
+    public bool FiftyFiftyLost { get; set; }
+    public int Balance { get; set; }
+    public int Pulls { get; set; }
+    public int PityCounter { get; set; }
+    public List<pulls_history> pullhist = new List<pulls_history>();
+    
+    public Player(int id,int balance)
     {
-        public int Balance
-        {
-            get => default;
-            set
-            {
-            }
-        }
+        ID = id;
+        Balance = balance;
+        pullhist= new List<pulls_history>();
+    }
 
-        public int Pulls
+    public void PerformPull(Banner banner)
+    {
+        if (Balance >= banner.Cost)
         {
-            get => default;
-            set
+            Balance -= banner.Cost;
+            Pulls++;
+
+            gacha_item itemToPull = banner.PullItem();
+
+            if (itemToPull != null)
             {
+                pulls_history entry = new pulls_history
+                {
+                    Item = itemToPull,
+                    Banner = banner,
+                    CreationDate = DateTime.Now,
+                    PullNumber = Pulls,
+                    FiftyFiftyLost = FiftyFiftyLost
+                };
+
+                pullhist.Add(entry);
+                UpdatePityAndFiftyFifty(itemToPull);
+
+                Console.WriteLine($"Player {ID} pulled {itemToPull.ItemRarity} from {banner.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"No items available to pull from {banner.Name}.");
             }
         }
+        else
+        {
+            Console.WriteLine($"Player {ID} doesn't have enough balance to perform a pull.");
+        }
+    }
+
+    public void WritePullHistoryToFile(string filePath)
+    {
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            foreach (var entry in pullhist)
+            {
+                string rarityString = GetRarityString(entry.Item.ItemRarity, entry.FiftyFiftyLost, entry.Banner is Permanent_banner);
+                string pullInfo = $"[{Environment.MachineName}] [{Environment.UserName}] " +
+                                 $"[{entry.PullNumber}] [{PityCounter}] [{entry.Item.ID}] {rarityString}";
+
+                writer.WriteLine(pullInfo);
+            }
+        }
+    }
+
+
+    private void UpdatePityAndFiftyFifty(gacha_item item)
+    {
+        if (item != null && item.ItemRarity == gacha_item.Rarity.Rare)
+        {
+            PityCounter = 0;
+            FiftyFiftyLost = false;
+        }
+        else
+        {
+            PityCounter++;
+            if (PityCounter >= 60)
+            {
+                PityCounter = 0;
+                FiftyFiftyLost = false;
+            }
+            else if (PityCounter == 59)
+            {
+                FiftyFiftyLost = true;
+            }
+        }
+    }
+
+
+    private string GetRarityString(gacha_item.Rarity rarity, bool fiftyFiftyLost, bool isPermanent)
+    {
+        if (rarity == gacha_item.Rarity.VeryCommon)
+        {
+            return "3-star";
+        }
+        else if (rarity == gacha_item.Rarity.Uncommon)
+        {
+            return "4-star";
+        }
+        else if (rarity == gacha_item.Rarity.Rare)
+        {
+            string bannerType = isPermanent ? "[PERM]" : "[EXCL]";
+            string fiftyFiftyResult = fiftyFiftyLost ? "[False]" : "[True]";
+            return $"5-star {bannerType} {fiftyFiftyResult}";
+        }
+        return string.Empty;
     }
 }
