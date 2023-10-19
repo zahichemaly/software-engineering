282492eb8090a511de43473aa8003bd7781c5a5c
zahichemaly <zahi.chemaly@gotocme.com>
2023-10-19 20:21:48 +0300

    added tp2 part3 for nai

---
+++ naihamdan-220607/tp2/GachaSystem/GachaSystem/Admin.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public class Admin:BaseUser
+    {
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public DateTime DateOfBirth
+        {
+            get;
+            set;
+        }
+
+        public int Balance
+        {
+            get;
+            set;
+        }
+
+        public int Pulls
+        {
+            get;
+            set;
+        }
+
+        public void updatePermanentBanner(PermanentBanner banner) { }
+
+        public void AddExclusiveBanner() { }
+
+        public void UpdateExclusiveBanner(ExclusiveBanner banner) { }
+
+        public void DeleteExclusiveBanner(ExclusiveBanner banner) { }
+
+        public void AddUser() { }
+
+        public void RemoveUser(BaseUser user) { }
+
+        public void UpdateSystemVersion(System system) { }
+
+        public override void validate()
+        {
+           
+        }
+    }
+}
