2df0195f08b733c87e1274f3bfac4adf631432a3
dana <dana.mshawrab@net.usj.edu.lb>
2023-10-19 17:56:10 +0300

    Added project and screenshots

---
+++ danamshawrab-220667/tp4/tp4-db/tp4-db/Controllers/CarsController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tp4_db.Models;
+using tp4_db.Repositories;
+
+namespace tp4_db.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly ICarRepository _carRepository;
+        public CarsController(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+        // GET: CarsController
+        public ActionResult Index()
+        {
+            return View(_carRepository.Get());
+        }
+
+       
+
+        // GET: CarsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        // POST: CarsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                _carRepository.Create(car);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(car);
+        }
+
+        // GET: CarsController/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var car = _carRepository.Get(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+        // POST: CarsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(string id, Car car)
+        {
+            if (id != car.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _carRepository.Update(id, car);
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(car);
+            }
+        }
+
+        // GET: CarsController/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var car = _carRepository.Get(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+
+        // POST: CarsController/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            var car = _carRepository.Get(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            _carRepository.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: CarsController/Details/5
+        public IActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var car = _carRepository.Get(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return View(car);
+        }
+
+
+    }
+}
---
+++ danamshawrab-220667/tp4/tp4-db/tp4-db/Repositories/ICarRepository.cs
@@ -0,0 +1,15 @@
+﻿using tp4_db.Models;
+
+namespace tp4_db.Repositories
+{
+    public interface ICarRepository
+    {
+        IEnumerable<Car> Get();
+        Car Get(string id);
+        Car Create(Car car);
+        void Update(string id, Car carIn);
+        void Remove(Car carIn);
+        void Remove(string id);
+
+    }
+}
