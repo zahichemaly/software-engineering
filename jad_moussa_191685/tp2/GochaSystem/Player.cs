2324a548b27102d0c37cdc1fb4da0ab9b662ff06
jad <jad.moussa@net.usj.edu.lb>
2023-10-26 19:16:08 +0300

    added jad folder

---
+++ jad_moussa_191685/tp2/GochaSystem/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+    public  class Player : BaseUser
+    {
+        public override void validate() { 
+        
+        
+        
+        }
+        private History history; 
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Balance { get; set; }
+        public int Pulls { get; set; }
+
+        public void PerformPull() { /* Implementation for performing a pull */ }
+        public void TrackPullHistory() { /* Implementation for tracking pull history */ }
+        public void TrackPityCounter() { /* Implementation for tracking pity counter */ }
+
+    }
+
+
+}
---
+++ jad_moussa_191685/tp2/GochaSystem/Version.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GachaSystem
+{
+
+    public class Version
+    {
+        public string VersionNumber { get; set; }
+        public int VersionCode { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
