c3b019fed6858f7a1e30086c13f1a3aa52baa73c
Wissam_Nasreddine <wissam.nasreddine@net.usj.edu.lb>
2023-11-02 19:42:47 +0200

    commited tp5 to part 4

---
+++ WissamNasreddine_222074/tp5/NewsBoard/NewsBoard.Nusiness/Models/GetAllNewsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NewsBoard.Business.DTOs;
+using NewsBoard.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsBoard.Business.Models
+{
+    public class GetAllNewsQuery:IRequest<IEnumerable<NewsDTO>>
+    {
+    }
+}
