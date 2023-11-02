177593abca076b46b6e15c48c4eccdc48b311e18
Elie Khoury <elie.kh222@hotmail.com>
2023-11-02 22:11:09 +0200

    tp5 part1-2-3-4

---
+++ eliegeorgioelkhoury-221069/tp5/NewsBoard/NewsBoard.API/Controllers/NewsController.cs
@@ -0,0 +1,72 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewsBoard.Business.Models;
+using NewsBoard.Data.Entities;
+using NewsBoard.Data.Repositories;
+
+namespace NewsBoard.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public NewsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<News>>> GetNews()
+        {
+            var query = new GetNewsQuery();
+            var news = await _mediator.Send(query);
+            return Ok(news);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] News model)
+        {
+            var command = new CreateNewsCommand()
+            {
+                News = model
+            };
+            var result = await _mediator.Send(command);
+            return Ok(new { Id = result });
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<News>> GetNewsById(string id)
+        {
+            var query = new GetNewsByIdQuery { NewsId = id };
+            var news = await _mediator.Send(query);
+
+            if (news == null)
+                return NotFound();
+
+            return Ok(news);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateNews(string id, [FromBody] News news)
+        {
+            if (id != news.Id)
+                return BadRequest();
+
+            var command = new UpdateNewsCommand { News = news };
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNews(string id)
+        {
+            var command = new DeleteNewsCommand { NewsId = id };
+            await _mediator.Send(command);
+
+            return NoContent();
+        }
+    }
+}
---
+++ eliegeorgioelkhoury-221069/tp5/NewsBoard/NewsBoard.Data/Repositories/INewsRepository.cs
@@ -0,0 +1,19 @@
+﻿using NewsBoard.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsBoard.Data.Repositories
+{
+    public interface INewsRepository
+    {
+        Task<IEnumerable<News>> Get();
+        Task<News> Get(string id);
+        Task Add(News news);
+        Task Update(News news);
+        Task Delete(string id);
+    }
+
+}
