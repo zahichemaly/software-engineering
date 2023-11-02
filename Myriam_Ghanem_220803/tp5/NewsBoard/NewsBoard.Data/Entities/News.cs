d0c0c778d1e816ee4339557f2291f5261ed616a0
Myriam-Ghanem <myriam.ghanem1@net.usj.edu.lb>
2023-11-02 19:18:42 +0200

    tp5

---
+++ Myriam_Ghanem_220803/tp5/NewsBoard/NewsBoard.Data/Entities/News.cs
@@ -0,0 +1,16 @@
+﻿public class News
+{
+    public News()
+    {
+        this.Id = Guid.NewGuid().ToString();
+        this.CreatedDate = DateTime.UtcNow;
+    }
+    public string Id { get; set; }
+    public string Headline { get; set; }
+    public string Article { get; set; }
+    public string CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? ModifiedDate { get; set; }
+    public int Rating { get; set; }
+    public int Views { get; set; }
+}
