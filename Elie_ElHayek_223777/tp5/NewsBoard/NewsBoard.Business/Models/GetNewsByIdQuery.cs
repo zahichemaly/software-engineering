e1b2b9609e693d6effcfcd953c3934326cd36106
elieHayek <elie.hayek2@net.usj.edu.lb>
2023-11-06 08:30:16 +0200

    tp5 part 5

---
+++ Elie_ElHayek_223777/tp5/NewsBoard/NewsBoard.Business/Models/GetNewsByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using NewsBoard.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsBoard.Business.Models
+{
+    public class GetNewsByIdQuery:IRequest<News>
+    {
+        public string NewsId { get; set; }  
+    }
+}
