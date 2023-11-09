e59a0a27e1722f98b1dd0915e52f6588bbce06f2
said-chebbo <said.chebbo@net.usj.edu.lb>
2023-11-09 19:41:25 +0200

    tp6

---
+++ Said_Chebbo_220700/tp6/eShop/BarcodeTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Class1
+{
+    [Fact]
+    public void Create_Should_Return_Null_When_Value_Is_Null()
+    {
+        string value = null;
+        var barcode = Barcode.Create(value);
+        Assert.Null(barcode);
+    }
+}
---
+++ Said_Chebbo_220700/tp6/eShop/src/eShop.Core/Entities/CreditCard.cs
@@ -0,0 +1,17 @@
+﻿namespace eShop.Core.Entities
+{
+    public class CreditCard
+    {
+        public CreditCardType Type { get; set; }
+        public string CardHolderName { get; set; }
+        public string Number { get; set; }
+        public DateTime Expiration { get; set; }
+        public string SecurityCode { get; set; }
+    }
+
+    public enum CreditCardType
+    {
+        VISA,
+        MASTERCARD
+    }
+}
