1441b6fbcccb1f65537ffa11d235c62552d68a6a
said-chebbo <said.chebbo@net.usj.edu.lb>
2023-11-16 19:29:28 +0200

    added tp7

---
+++ Said_Chebbo_220700/tp7/eShop/src/eShop.Core/Models/ErrorModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace eShop.Core.Models
+{
+    public class ErrorModel
+    {
+        [JsonPropertyName("code")]
+        public string Code { get; set; }
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
