04686c3fe0f4eb5c95e6ff9a49bcbd4f744fbc51
Elie-Haouchan <elie.haouchan@net.usj.edu.lb>
2023-11-16 19:22:38 +0200

    Added tp7

---
+++ ElieHaouchan-220561/tp7/eShop/src/eShop.Core/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿namespace eShop.Core.Entities
+{
+    public class Order
+    {
+        public Basket Basket { get; private set; }
+        public Contact Contact { get; set; }
+        public CreditCard CreditCard { get; set; }
+
+        public Order(Basket basket, Contact contact, CreditCard creditCard)
+        {
+            Basket = basket;
+            Contact = contact;
+            CreditCard = creditCard;
+        }
+    }
+}
